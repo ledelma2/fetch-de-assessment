1a28e96057ea284a91305a821d219d951c43fb8d
Liam Edelman <ledelman1995@gmail.com>
2025-01-10 11:46:01 -0600

    Update docker-compose-up.sh

--- env/scripts/sh/docker-compose-up.sh
+++ env/scripts/sh/docker-compose-up.sh
@@ -1,2 +1,2 @@
 echo "Running Docker Compose Up..."
-docker compose -f ../../docker-compose.yml up
+docker compose -f ../../docker/docker-compose.networks.yml -f ../../docker/docker-compose.kafka.yml -f ../../docker/docker-compose.producer.yml up
